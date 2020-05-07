e567f47efc5908ae478e2f12d4659f05988f9b8b
kuroko <kurosawa.yk@gmail.com>
2020-05-07 16:19:06 +0900

    update yolov3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name="torchyolo",
+    name="torch_yolo",
     packages=find_packages(),
     install_requires=["numpy", "torch", "matplotlib", "terminaltables", "pillow", "tqdm"],
     version="0.1"
