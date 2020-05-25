fd064aeefcc58f8538e1c4f7c8fbbeb3cf5f1e6e
kuroko <kurosawa.yk@gmail.com>
2020-05-25 19:12:04 +0900

    update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from __future__ import division
 
-from models import *
-from utils.utils import *
-from utils.datasets import *
-from utils.parse_config import *
+from torch_yolo.models import *
+from torch_yolo.utils.utils import *
+from torch_yolo.utils.datasets import *
+from torch_yolo.utils.parse_config import *
 
 import os
 import sys
@@ -18,6 +18,10 @@ from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
+import nne
+print('koko')
+import time
+#time.sleep(10)
 
 
 def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
@@ -54,3 +58,57 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
 
     return precision, recall, AP, f1, ap_class
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+    parser.add_argument("--num_classes", type=int, default="config/yolov3.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
+    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
+    parser.add_argument("--iou_thres", type=float, default=0.5, help="iou threshold required to qualify as detected")
+    parser.add_argument("--conf_thres", type=float, default=0.001, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    opt = parser.parse_args()
+    print(opt)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    data_config = parse_data_config(opt.data_config)
+    valid_path = data_config["valid"]
+    class_names = load_classes(data_config["names"])
+
+    # Initiate model
+    model = Darknet(num_classes=opt.num_classes, version="v3").to(device)
+    print(model)
+    if opt.weights_path.endswith(".weights"):
+        # Load darknet weights
+        model.load_darknet_weights(opt.weights_path)
+    else:
+        # Load checkpoint weights
+        model.load_state_dict(torch.load(opt.weights_path))
+
+    print('onn')
+    nne.cv2onnx(model, (8, 3, 416, 416), "yolo.onnx")
+    #nne.cv2tflite(model, (8, 3, 416, 416), "yolo.tflite")
+    #nne.cv2trt(model, (8, 3, 416, 416), "yolo.trt")
+
+    print("Compute mAP...")
+
+    precision, recall, AP, f1, ap_class = evaluate(
+        model,
+        path=valid_path,
+        iou_thres=opt.iou_thres,
+        conf_thres=opt.conf_thres,
+        nms_thres=opt.nms_thres,
+        img_size=opt.img_size,
+        batch_size=8,
+    )
+
+    print("Average Precisions:")
+    for i, c in enumerate(ap_class):
+        print(f"+ Class '{c}' ({class_names[c]}) - AP: {AP[i]}")
+
+    print(f"mAP: {AP.mean()}")
